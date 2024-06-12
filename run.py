aa03486d68d43b0595ffa2a072faef41d42982c4
MariusMphy <marius.markelis@gmail.com>
2024-06-12 21:30:14 +0300

    changed port

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from car_repair_shop_project import app, db
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
