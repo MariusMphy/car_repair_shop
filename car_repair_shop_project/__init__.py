f907a2ccd74166d4d79151ba09a93e7e8f94b400
MariusMphy <marius.markelis@gmail.com>
2024-06-12 19:43:33 +0300

    secret_key to env

--- car_repair_shop_project/__init__.py
+++ car_repair_shop_project/__init__.py
@@ -4,9 +4,8 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 
-
 app: Flask = Flask(__name__)
-app.config['SECRET_KEY'] = '5sfjshgfjkhsgf554654saffasjask'
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI']: str = 'sqlite:///car_repair.db'
 db: SQLAlchemy = SQLAlchemy(app)
 
